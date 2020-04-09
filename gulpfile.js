9c144e070e47ffaef1544962dad43959408a2d4c
nabeelatgit <kpnamo@gmail.com>
2020-04-09 15:06:23 +0530

    Adding html, scss and images to the repository of huddle landing page from front end mentor

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('compile', function() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
